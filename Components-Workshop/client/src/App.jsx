088c47a3e74fc1a80ebe97ed309922c5e925a2c5
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-11-07 17:34:20 +0200

    workshop

--- Components-Workshop/client/src/App.jsx
+++ Components-Workshop/client/src/App.jsx
@@ -1,12 +1,15 @@
 import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import UserList from "./components/UserList";
 
 function App() {
 	return (
 		<div>
 			<Header />
-
+			<main className="main">
+				<UserList />
+			</main>
 			<Footer />
 		</div>
 	);
