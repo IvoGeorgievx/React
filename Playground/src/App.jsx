c90add1f7e792fbc087ae518141d825d5fa98132
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-11-18 17:14:51 +0200

    playground

--- Playground/src/App.jsx
+++ Playground/src/App.jsx
@@ -1,9 +1,18 @@
-import Navigation from "./components/Navigation";
+import Navigation from "./components/Navigation/Navigation";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contacts from "./components/Contacts/Contacts";
 
 export default function App() {
 	return (
 		<>
 			<Navigation />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contacts" element={<Contacts />} />
+			</Routes>
 		</>
 	);
 }
