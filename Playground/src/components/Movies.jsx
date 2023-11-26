e06972c2de77bdf72f2ca5c5f8dd703f57313dbe
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-11-26 14:00:05 +0200

    playground

--- Playground/src/components/Movies.jsx
+++ Playground/src/components/Movies.jsx
@@ -3,27 +3,21 @@ import { ALL_MOVIES_URL } from "../api/urls";
 
 export default function Movies() {
 	const [movies, setMovies] = useState([]);
-
-	const movieHandler = () => {
+	useEffect(() => {
 		fetchMovies();
-	};
+	}, []);
+
 	const fetchMovies = () => {
 		fetch(ALL_MOVIES_URL)
 			.then((res) => res.json())
 			.then((data) => {
 				setMovies(data);
 			});
-		console.log(movies);
 	};
-
-	// useEffect(() => {
-	// 	fetchMovies();
-	// }, []);
 
 	return (
 		<>
 			<h1>Here to fetch the movies from the DB</h1>
-			<input type="submit" value="Fetch Movies" onClick={movieHandler} />
 			<ul>
 				{movies.map((movie) => (
 					<li key={movie.id}>{movie.name}</li>
---
+++ Playground/src/components/hooks/usePersistedState.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function usePersistedState(key, defaultValue) {
+	const [state, setState] = useState(() => {
+		const persistedState = localStorage.getItem(key);
+
+		if (persistedState) {
+			return JSON.parse(persistedState);
+		}
+
+		return defaultValue;
+	});
+
+	const setPersistedState = (value) => {
+		setState(value);
+		const serializedValue = JSON.stringify(value);
+		localStorage.setItem(key, serializedValue);
+	};
+
+	return [state, setPersistedState];
+}
