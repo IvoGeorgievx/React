48fdb4f2bb478b0707a8353e8fcdc46a4e093abc
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-12-01 21:39:01 +0200

    playground

--- MoviesFE/src/components/Header/Header.jsx
+++ MoviesFE/src/components/Header/Header.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
+import { RightSquareOutlined, LeftSquareOutlined } from "@ant-design/icons";
+
 import styles from "./Header.module.css";
 import Navigation from "./Navigation";
+import DrawerModal from "../Modals/DrawerModal";
 
 export default function Header() {
+	const [shown, setShown] = useState(false);
+	const showHandler = () => {
+		setShown(!shown);
+	};
+
 	return (
 		<>
 			<div className={styles["nav-logo"]}>
 				<img className={styles["logo-img"]} src="/images/logo.jpg" alt="logo" />
+				{shown ? (
+					<>
+						<LeftSquareOutlined
+							style={{ fontSize: "25px", color: "white" }}
+							onClick={showHandler}
+						/>
+					</>
+				) : (
+					<RightSquareOutlined
+						style={{ fontSize: "25px", color: "white" }}
+						onClick={showHandler}
+					/>
+				)}
 				<Navigation />
 			</div>
+			<DrawerModal />
 		</>
 	);
 }
