617632ccacda2879c4682b6443b40c455d20a232
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2023-12-04 20:15:09 +0200

    playground

--- MoviesFE/src/components/Home/Home.jsx
+++ MoviesFE/src/components/Home/Home.jsx
@@ -2,8 +2,32 @@ import styles from "./Home.module.css";
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Where Movies Happen</h1>
+		<div className={styles["home-wrapper"]}>
+			<div>
+				<h1 className={styles["main-home-heading"]}>Where Movies Happen.</h1>
+			</div>
+			<div className={styles["home-wrapper-img"]}>
+				<img
+					className={styles["posters-many"]}
+					src="/images/home-pic.jpg"
+					alt="movie"
+				/>
+				<img
+					className={styles["poster-gladiator"]}
+					src="/images/gladiator.png"
+					alt="movie"
+				/>
+				{/* <img
+					className={styles["poster-rocky"]}
+					src="/images/rocky.webp"
+					alt="movie"
+				/> */}
+				<img
+					className={styles["poster-interstellar"]}
+					src="/images/interstellar.jpg"
+					alt="movie"
+				/>
+			</div>
 		</div>
 	);
 }
